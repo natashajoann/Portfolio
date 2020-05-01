701f8479edba4a6841c9b51abf445240423bb760
Natasha Verdon <natashamverdon@gmail.com>
2020-05-01 16:56:01 -0700

    js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ var slideIndex = 1;
       slideIndex++;
       if (slideIndex > slides.length) {slideIndex = 1}
       slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides, 4000); // Change image every 2 seconds
+      setTimeout(showSlides, 8000); // Change image every 2 seconds
   }
 
 });

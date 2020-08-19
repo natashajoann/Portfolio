a0585d39870d021b1b80de670047319aa6f709dc
Natasha Verdon <natashamverdon@gmail.com>
2020-08-19 15:00:15 +0200

    activate

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 // BURGER menu
 
    $(".burger-button").click(function(){
-     $(".burger-button").toggleClass("active");
+     $(".burger-button").toggleClass("activate");
      $(".burger-menu").slideToggle();
    });
 
 
    $(".burger-menu").click(function(){
-     $(".burger-button").toggleClass("active");
+     $(".burger-button").toggleClass("activate");
      $(".burger-menu").slideToggle();
    });
 

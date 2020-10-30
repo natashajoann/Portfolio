a8b7a9b126e772d06f221db3448acf8c16a07815
Natasha Verdon <natashamverdon@gmail.com>
2020-10-30 09:14:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,68 @@
 
-   $( document ).ready(function() {
-   $(".dropdown-nav").click(function(){
-       $(this).children(".dropdown-content").slideToggle("200");
-     });
-   });
+(function($) {
 
-   var acc = document.getElementsByClassName("accordion");
-     var i;
+    $(document).ready(function() {
 
-     for (i = 0; i < acc.length; i++) {
-       acc[i].addEventListener("click", function() {
-         this.classList.toggle("active");
-         var panel = this.nextElementSibling;
-         if (panel.style.maxHeight) {
-           panel.style.maxHeight = null;
-         } else {
-           panel.style.maxHeight = panel.scrollHeight + "px";
-         }
-       });
-     }
+        $('.hamburger__container').click(function() {
+            $('.nav').toggleClass('open');
+            $(this).children().first().toggleClass('open');
+            $('html, body').toggleClass('noscroll');
+        });
 
-     $(document).ready(function(){
-       $('.slick').slick({
-         slidesToShow: 3,
-         slidesToScroll: 1,
-         autoplay: true,
-         autoplaySpeed: 2000,
-       });
-     });
+        // Bacon Ipsum
+        $.ajax({
+            url: 'https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1&format=html',
+            success: function(data) {
+                $('#content').html(data);
+            }
+        });
 
-var slideIndex = 1;
-  showSlides(slideIndex);
+    });
 
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
 
-  function showSlides() {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
+    //SLIDES
+    var slideIndex = 1;
+      showSlides(slideIndex);
+
+      // Next/previous controls
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
       }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}
-      slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides, 8000); // Change image every 2 seconds
-  }
+
+      // Thumbnail image controls
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+      function showSlides() {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";
+          }
+          slideIndex++;
+          if (slideIndex > slides.length) {slideIndex = 1}
+          slides[slideIndex-1].style.display = "block";
+          setTimeout(showSlides, 6000);
+      }
+
+      $(".dropdown-nav").click(function(){
+      $(this).children(".dropdown-content").slideToggle("200");
+    });
 
 
-
-});
+})(jQuery);

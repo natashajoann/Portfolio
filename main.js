391c3d489046b44a53a703a5791f354be30c9be4
Natasha Verdon <natashamverdon@gmail.com>
2020-10-30 09:13:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -57,21 +57,3 @@ var slideIndex = 1;
 
 
 });
-
-$(document).ready(function() {
-
-
-  $('.hamburger__container').click(function() {
-              $('.nav').toggleClass('open');
-              $(this).children().first().toggleClass('open');
-              $('html, body').toggleClass('noscroll');
-          });
-
-          // Bacon Ipsum
-          $.ajax({
-              url: 'https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1&format=html',
-              success: function(data) {
-                  $('#content').html(data);
-              }
-          });
-});

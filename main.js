677316ed621e04e7f93f28a2004f1b5cd2646b9f
Natasha Verdon <natashamverdon@gmail.com>
2020-12-22 12:39:17 +0100

    slick

--- main.js
+++ main.js
@@ -47,31 +47,6 @@ $('.single-item').slick({
   autoplaySpeed: 10000,
   });
 
-    //SLIDES
-    var slideIndex = 1;
-      showSlides(slideIndex);
-
-      // Next/previous controls
-      function plusSlides(n) {
-        showSlides(slideIndex += n);
-      }
-
-      // Thumbnail image controls
-      function currentSlide(n) {
-        showSlides(slideIndex = n);
-      }
-
-      function showSlides() {
-          var i;
-          var slides = document.getElementsByClassName("mySlides");
-          for (i = 0; i < slides.length; i++) {
-              slides[i].style.display = "none";
-          }
-          slideIndex++;
-          if (slideIndex > slides.length) {slideIndex = 1}
-          slides[slideIndex-1].style.display = "block";
-          setTimeout(showSlides, 6000);
-      }
 
       $(".dropdown-nav").click(function(){
       $(this).children(".dropdown-content").slideToggle("200");

15c3ac66a246af25d14d3c3e4792a899bb6df105
Natasha Verdon <natashamverdon@gmail.com>
2020-12-22 12:07:44 +0100

    slick

--- main.js
+++ main.js
@@ -34,6 +34,19 @@
       });
     }
 
+    // Slick Slider
+    $('.autoplay').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 10000,
+    });
+
+$('.single-item').slick({
+  autoplay: true,
+  autoplaySpeed: 10000,
+  });
+
     //SLIDES
     var slideIndex = 1;
       showSlides(slideIndex);

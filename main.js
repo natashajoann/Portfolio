0837bf927d86bfd75eb17ebf6aaec779858af822
Natasha Verdon <natashamverdon@gmail.com>
2021-03-27 12:11:33 +0100

    latest

--- main.js
+++ main.js
@@ -10,6 +10,32 @@
         autoplaySpeed: 10000,
       });
 
+      $('.center').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+
         $('.hamburger__container').click(function() {
             $('.nav').toggleClass('open');
             $(this).children().first().toggleClass('open');
